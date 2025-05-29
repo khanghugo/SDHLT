5556cc08b1188845a576ea017737314643b19de5
khang <mkhangle20@gmail.com>
2025-05-29 11:13:01 -0400

    more world extent

--- src/sdhlt/common/bspfile.h
+++ src/sdhlt/common/bspfile.h
@@ -87,7 +87,7 @@
 #define TEXTURE_STEP        16 // this constant was previously defined in lightmap.cpp. --vluzacn
 #define MAX_SURFACE_EXTENT  16 // if lightmap extent exceeds 16, the map will not be able to load in 'Software' renderer and HLDS. //--vluzacn
 
-#define ENGINE_ENTITY_RANGE 65536.0
+#define ENGINE_ENTITY_RANGE 262144.0
 //=============================================================================
 
 #define BSPVERSION  30
